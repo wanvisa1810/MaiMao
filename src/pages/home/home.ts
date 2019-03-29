1ea38e0631507b268fa30983702fc958874dddef
Wanvisa Suwannin <wanvisa.nun@gmail.com>
2019-03-29 16:18:14 +0700

    update app,home,tabs

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+
+import{ Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+  customer:any=0;
+  constructor(public navCtrl: NavController, public navParam: NavParams, public http: Http){
+    this.http.get('http://localhost:8080/customer')
+    .map(res=>res.json())
+    .subscribe(data=>{
+    this.customer = data;
+     
+  });
 
   }
 
