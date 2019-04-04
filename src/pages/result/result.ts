d9673921a516da0e8f44fec0717f068df49d7a2d
Wanvisa Suwannin <wanvisa.nun@gmail.com>
2019-04-04 16:23:59 +0700

    add detail result

--- src/pages/result/result.ts
+++ src/pages/result/result.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { DetailPage } from '../detail/detail';
+import 'rxjs/add/operator/map';
+import {Http} from '@angular/http';
 
 /**
  * Generated class for the ResultPage page.
@@ -14,10 +17,17 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'result.html',
 })
 export class ResultPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  customer:any=0;
+  constructor(public navCtrl: NavController, public navParams: NavParams,public http: Http) {
+    let customerID=this.navParams.get('keyword');
+    let url = "http://localhost:8080/customer/name/" + customerID;
+    console.log(url)
+    this.http.get(url).map(res => res.json()).subscribe(data => {this.customer = data});
   }
-
+  showDetail(id)
+  {
+    this.navCtrl.push(DetailPage,{customerID:id});
+  }
   ionViewDidLoad() {
     console.log('ionViewDidLoad ResultPage');
   }
